4fb2a560e02f247c275b49e57a0e835486da7e5d
tdiwenocur <diwenocur@ucdavis.edu>
2016-02-04 18:10:58 -0800

    Initial commit

---
+++ interactive.js
@@ -0,0 +1,162 @@
+$(document).ready(function(){
+    
+    console.log("ready");
+
+//hide the b and white photos
+$('#bandwphotos').hide();
+$('#captions p').hide();	
+$('#bigphotos img').hide();
+
+	$('#title').click(function(){
+		$('#bandwphotos').show();
+		return false;
+		
+	})
+//hover the headphones pic	
+$('#headpic').hover(function(){
+console.log("hovered headphones");
+	$('p#headphones').show();
+	},
+	function(){
+		$('p#headphones').hide();
+});	
+
+//hover the pencil pic	
+$('#penpic').hover(function(){
+console.log("hovered pencil");
+	$('p#pencil').show();
+	},
+	function(){
+		$('p#pencil').hide();
+});	
+
+//hover the phone pic	
+$('#phonepic').hover(function(){
+console.log("hovered phone");
+	$('p#phone').show();
+	},
+	function(){
+		$('p#phone').hide();
+});	
+
+//hover the bikelight pic	
+$('#bikepic').hover(function(){
+console.log("hovered bikepic");
+	$('p#bikelight').show();
+	},
+	function(){
+		$('p#bikelight').hide();
+});	
+
+//hover the glove pic	
+$('#glovepic').hover(function(){
+console.log("hovered glove");
+	$('p#gloves').show();
+	},
+	function(){
+		$('p#gloves').hide();
+});	
+
+
+//LET THE COLORS COME
+
+//blow up the headphones pic	
+$('#headpic').click(function(){
+console.log("clicked pencil");
+$('#headphonecol').show();
+	$('#headphonecol').animate({
+		left: "0%",
+	}
+	
+	);
+});
+//unclick headphones
+$('#headphonecol').click(function(){
+console.log("clicked body");
+$('#headphonecol').animate({
+	left: "100%",
+});
+});
+
+//blow up the pencil pic	
+$('#penpic').click(function(){
+console.log("clicked pencil");
+$('#pencilcol').show();
+	$('#pencilcol').animate({
+		left: "0%",
+	}
+	
+	);	
+});
+//unclick headphones
+$('#pencilcol').click(function(){
+console.log("clicked body");
+$('#pencilcol').animate({
+	left: "100%",
+});
+});	
+//blow up the phone pic	
+$('#phonepic').click(function(){
+console.log("clicked pencil");
+$('#phonecol').show();
+	$('#phonecol').animate({
+		left: "0%",
+	}
+	
+	);	
+});
+//unclick headphones
+$('#phonecol').click(function(){
+console.log("clicked body");
+$('#phonecol').animate({
+	left: "100%",
+});
+});
+
+//blow up the bike light pic	
+$('#bikepic').click(function(){
+console.log("clicked pencil");
+$('#bikelightcol').show();
+	$('#bikelightcol').animate({
+		left: "0%",
+	}
+	
+	);	
+});
+//unclick bikelight
+$('#bikelightcol').click(function(){
+console.log("clicked body");
+$('#bikelightcol').animate({
+	left: "100%",
+});
+});
+//blow up the gloves pic	
+$('#glovepic').click(function(){
+console.log("clicked pencil");
+$('#glovescol').show();
+	$('#glovescol').animate({
+		left: "0%",
+	}
+	
+	);	
+});	
+//unclick headphones
+$('#glovescol').click(function(){
+console.log("clicked body");
+$('#glovescol').animate({
+	left: "100%",
+});
+});
+	
+	
+	
+	//close jquery
+	})
+	
+
+	
+	
+	
+	
+	
+	
